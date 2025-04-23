aa284a2f738a4a9a3e1dd2e7f9bede11e39b4d03
Ben Keller <malzraa@gmail.com>
2025-04-23 17:24:09 -0500

    Wrote a bunch of regular expressions to strip out non-words and added the option to skip pages.

--- indexer.py
+++ indexer.py
@@ -1,11 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import re
+import string
 import argparse
 from functools import reduce
 from pypdf import PdfReader
 
 def count_page(pwords, word):
-    indices = [i for i in range(len(pwords)) if word in pwords[i]]
+    indices = [i+1 for i in range(len(pwords)) if word in pwords[i]]
     return indices
+
+def word_checker(word):
+    has_letter = re.compile('[a-zA-Z]')
+    if has_letter.match(word) is None:
+        return False
+    return True
+
+def split_words(text):
+    text = re.sub(r'\s+', ' ', text).strip() # remove extra spaces
+    text = re.sub(r'- ', '', text).strip() # merge hyphenated line-broken words
+    text = text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+    text = re.sub(r'([a-zA-Z])[0-9]', r'\1', text) # remove trailing numbers
+    return text.split(' ')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -13,14 +28,15 @@ if __name__ == "__main__":
             description='Generate an index from a PDF proof'
             )
     parser.add_argument('filename')
+    parser.add_argument('-s', '--skip', type=int, default=0,
+            help='Skip the first N pages')
     args = parser.parse_args()
 
     reader = PdfReader(args.filename)
-    N_page = len(reader.pages)
     page_words = []
-    for page in reader.pages:
-        text = page.extract_text()
-        page_words.append(set(text.replace(r'\n', ' ').split(' ')))
+    for page in reader.pages[args.skip:]:
+        words = split_words(page.extract_text())
+        page_words.append(set(filter(word_checker, words)))
     all_words = reduce(lambda x,y: x|y, page_words)
     index = {w:count_page(page_words, w) for w in all_words}
     for w in all_words:
